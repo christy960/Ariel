ab8c69ad68bb3ca7f0a69e0698523d51b7927185
christy960 <ashlinchristy61@gmail.com>
2022-02-22 16:29:43 +0530

    version5

--- ariel/settings.py
+++ ariel/settings.py
@@ -127,4 +127,8 @@ STATICFILES_DIR=[
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStatiFileAStorage"
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT=BASE_DIR/'static'
+
+
+    
