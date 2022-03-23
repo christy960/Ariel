0728a87b6fb4f3472b78cf138487f99548e28b6e
christy960 <ashlinchristy61@gmail.com>
2022-03-23 15:29:10 +0530

    version9

--- rex/urls.py
+++ rex/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('home',views.home,name='home'),
     path('signup',views.signup,name='signup'),
     path('signup6',views.signup6,name='signup6'),
-    path('test',views.signup4,name='signup4')
-
+    path('test',views.signup4,name='signup4'),
+    path('login/',views.login,name='login'),
+    path('viewUsers/',views.viewUSers,name='viewUsers'),
+    path('updateuser/<int:id>',views.updateUser,name='updateuser'),
+    path('deleteuser/<int:id>',views.deleteUser,name='deleteuser'),
 ]
--- rex/views.py
+++ rex/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import users
-from .models import Users
+
 
 
 def index(request):
     return render(request,'vulko.html')
 def home(request):
-    return render(request,'morgana.html')
+    id=request.session['userid']
+    userdata=users.objects.get(id=id)
+    return render(request,'morgana.html',{"userdetails":userdata})
+
 def signup(request):
     return render(request,'sign.html')
 def signup6(request):
@@ -25,7 +28,49 @@ def signup4(request):
         
          return render(request,'test.html')  
      else:
-         return render(request,'test.html')      
+         return render(request,'test.html')    
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        try:
+            data=users.objects.get(username=username, paswword=password)
+            request.session['userid']=data.id
+            return redirect('/home')
+        except users.DoesNotExist:
+            return render(request,'oblivion.html',{'status':'login failed'})    
+    else:
+              return render(request,'oblivion.html') 
+
+
+def viewUSers(request):
+    userdata=users.objects.all()
+    return render(request,'viewUsers.html',{"userdata": userdata}) 
+
+def updateUser(request, id):
+    if request.method =="POST":
+        fullname=request.POST['fullname']
+        address=request.POST['address']
+        age=request.POST['age']
+        username=request.POST['username']
+        password=request.POST['password']
+        userdetils=users.objects.filter(id=id).update(fullname=fullname, address=address, paswword=password,age=age,username=username)
+        return redirect('/viewUsers/')
+    else:
+        userdata=users.objects.get(id=id)
+        return render(request,'updateuser.html', {"userdetails": userdata}) 
+
+def deleteUser(request, id):
+    users.objects.filter(id=id).delete()
+    return redirect('/viewUsers/')
+
+      
+    
+
+ 
+
+         
  
  
 # Create your views here.
